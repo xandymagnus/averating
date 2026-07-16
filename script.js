519873c310fa89adcc9b2c4e93959f5e51d3110b
xandymagnus <xandy.magno325@gmail.com>
2026-07-15 21:05:02 -0300

    adding comments

--- script.js
+++ script.js
@@ -1,15 +1,19 @@
+// GET PAGES
 const homePage = document.getElementById("home");
 const defaultPage = document.getElementById("default-page");
 const advancedPage = document.getElementById("advanced-page");
 
+// GET BUTTONS
 const btn_default = document.querySelector(".btn_default");
 const btn_advanced = document.querySelector(".btn_advanced");
 
+// DEFAULT DISPLAY
 btn_default.addEventListener("click", () => {
     homePage.classList.add("hidden");
     defaultPage.classList.remove("hidden");
 });
 
+// ADVANCED DISPLAY
 btn_advanced.addEventListener("click", () => {
     homePage.classList.add("hidden");
     advancedPage.classList.remove("hidden");
